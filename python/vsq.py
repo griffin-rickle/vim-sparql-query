0116b0c38384ad6c9810b9af4ce4df2ddf714ecc
Griff <rickleg.93@gmail.com>
2023-01-24 21:47:41 -0500

    Got reading from a config file working

--- python/vsq.py
+++ python/vsq.py
@@ -1,27 +1,31 @@
+import json
 import os
 import requests
 import sys
 import vim
 
+
+def get_config():
+    python_dir_ary = sys.path[0].split(os.path.sep)
+    config_dir = os.path.sep.join(python_dir_ary[:len(python_dir_ary) - 1])
+    config_filepath = os.path.sep.join([config_dir, 'config.json'])
+    with open(config_filepath, 'r') as f:
+        config = json.load(f)
+    return config
+
+
 def buffer_query():
+    endpoint_config = get_config()
+    print(json.dumps(endpoint_config, indent=4))
 
-    params = {
-        "run": ' Run Query ',
-        "timeout": 30000,
-        "default_graph_uri": '',
-        "format": "text/plain",
+    query_endpoint = endpoint_config['endpoint']
+    auth = (endpoint_config['auth']['username'], endpoint_config['auth']['password'])
 
+    data = {
+        "query": '\n'.join(vim.current.buffer)
     }
-    buffer_query = '\n'.join(vim.current.buffer)
-    params['query'] = buffer_query
-    endpoint = "https://lod.openlinksw.com/sparql/"
 
     vim.command(":new")
-
-    # vim.command("sblast")
-    tmp_file = "/tmp/test.sq"
-
-    result = requests.get(endpoint, params=params)
-
+    result = requests.post(query_endpoint, data=data, auth=auth, headers=endpoint_config['headers'])
     vim.current.window.buffer[:] = result.text.split('\n')
     vim.current.window.buffer.options['buftype'] = 'nofile'
