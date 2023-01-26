357afc1e34e9e222aae5d5a70404f8f8efb535b9
Griff <rickleg.93@gmail.com>
2023-01-25 21:05:54 -0500

    Different accept types per query type Updates the same buffer instead of opening a new one every time

--- python/vsq.py
+++ python/vsq.py
@@ -5,6 +5,8 @@ import sys
 import vim
 
 
+results_buffer = None
+
 def get_config():
     python_dir_ary = sys.path[0].split(os.path.sep)
     config_dir = os.path.sep.join(python_dir_ary[:len(python_dir_ary) - 1])
@@ -14,9 +16,19 @@ def get_config():
     return config
 
 
+def get_query_type():
+    curr_index = 0
+    curr_line = vim.current.buffer[0]
+    while curr_index < len(vim.current.buffer) and curr_line.startswith('#'):
+        curr_index += 1
+        curr_line = vim.current.buffer[curr_index]
+    query_keyword = curr_line.split(' ')[0]
+    return query_keyword
+
+
 def buffer_query():
+    global results_buffer
     endpoint_config = get_config()
-    print(json.dumps(endpoint_config, indent=4))
 
     query_endpoint = endpoint_config['endpoint']
     auth = (endpoint_config['auth']['username'], endpoint_config['auth']['password'])
@@ -25,7 +37,16 @@ def buffer_query():
         "query": '\n'.join(vim.current.buffer)
     }
 
-    vim.command(":new")
-    result = requests.post(query_endpoint, data=data, auth=auth, headers=endpoint_config['headers'])
-    vim.current.window.buffer[:] = result.text.split('\n')
-    vim.current.window.buffer.options['buftype'] = 'nofile'
+    query_type = get_query_type()
+    headers = {}
+    if query_type in endpoint_config['headers'].keys():
+        headers = endpoint_config['headers'][query_type]
+
+    if results_buffer is None:
+        vim.command(":new")
+        vim.current.window.buffer.options['buftype'] = 'nofile'
+        results_buffer = vim.current.buffer
+
+    result = requests.post(query_endpoint, data=data, auth=auth, headers=headers)
+    results_buffer[:] = result.text.split('\n')
+
